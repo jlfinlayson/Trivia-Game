a52d698cffdff0f2ed613b1db0f92732f1acad63
jess <jlfinlayson4@gmail.com>
2019-07-08 17:28:36 -0400

    "basic html format and questions added to java"

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+// Trivia Questions and Answers
+
+var questions = [{
+    question: "What’s the name of Napoleon’s llama in 'Napoleon Dynamite'?",
+    choices: ["Pedro", "Gina", "Tina", "LaFawnduh"],
+    answer: 2,
+},
+{
+    question: "Which Destiny's Child member was the first to have a solo Billboard No. 1 single?",
+    choices: ["Beyonce", "Michelle Williams", "LaTavia Roberson", "Kelly Rowland"],
+    answer: 3,
+},
+{
+    question: "What is Myspace Tom’s full name?",
+    choices: ["Tom Anderson", "Thomas Baker", "Tom Baker", "Thomas Jones"],
+    answer: 0,
+},
+{
+    question: "In 'Mean Girls,' which Missy Elliot song is playing when Janis and Damian tell Cady about the Plastics?",
+    choices: ["Work It", "Pass That Dutch", "Get Ur Freak On", "Lose Control"],
+    answer: 1,
+},
+{
+    question: "What did Paris Hilton think Walmart sold?",
+    choices: ["Children's Toys", "Pets", "Wall Stuff", "She didn't think it was a real store"],
+    answer: 2,
+},
+{
+    question: "Who was the first American Idol winner",
+    choices: ["Ruben Studdard", "Justin Guarini", "Clay Aiken", "Kelly Clackson"],
+    answer: 3,
+},
+{
+    question: "What was the first video uploaded to YouTube?",
+    choices: ["Badass Snowboarding", "Me at the zoo", "Never Gonna Give You Up", "Flambe Disaster"],
+    answer: 1,
+},
+{
+    question: "8. How many weeks was Eminem’s “Lose Yourself” No. 1 on Billboard?",
+    choices: ["10", "14", "8", "12"],
+    answer: 1,
+},
+];
